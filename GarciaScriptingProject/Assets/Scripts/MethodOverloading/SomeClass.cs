efe66abf88abd631fdd7791c70e01302e419118f
Xx-Diego-XX-Agustin-xx-Garcia-xX <720791@student.dusd.net>
2024-01-26 12:38:03 -0800

    Completed Method Overloading
    Completed the "Method Overloading" assignment, which implemented elements to confirm my proficiency in the field of method overloading.

---
+++ GarciaScriptingProject/Assets/Scripts/MethodOverloading/SomeClass.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeClass : MonoBehaviour
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public string Add(string a, string b)
+    {
+        return a + b;
+    }
+}
---
+++ GarciaScriptingProject/Assets/Scripts/MethodOverloading/SomeOtherClass.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeOtherClass : MonoBehaviour
+{
+    void Start()
+    {
+        SomeClass myClass = new SomeClass();
+        myClass.Add(1, 2);
+        myClass.Add("Hello ", "World");
+    }
+}
